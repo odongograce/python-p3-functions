4b12a3cf92b6b4045c637794d22a8f101e4bfaa0
Grace Odongo <graceakinyi024@gmail.com>
2025-11-29 23:45:14 +0300

    new

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    print(num1 + num2)
     pass
 
 def halve(number):
+    print(50/2)
     pass
+
+add(45,55)
